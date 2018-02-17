a4f202cbce2d649edf43c6d97814f70512e8c21e
Filippo De Luca <filippo.deluca@ovoenergy.com>
2018-02-17 10:23:37 +0000

    feat: Update the build to SBT 1.x
    Update the build and plugins to SBT 1.x
    It also recreate all the file headers

--- avro4s/src/test/scala/com/ovoenergy/kafka/serialization/avro4s/Avro4sSerializationSpec.scala
+++ avro4s/src/test/scala/com/ovoenergy/kafka/serialization/avro4s/Avro4sSerializationSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 OVO Energy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ovoenergy.kafka.serialization.avro4s
 
 import java.io.{ByteArrayOutputStream, OutputStream}
@@ -37,7 +53,8 @@ class Avro4sSerializationSpec extends UnitSpec with WireMockFixture {
             whenever(events.length > 1) {
               // This verifies that the HTTP cal to the schema registry happen only once.
               testWithPostSchemaExpected(s"$topic-value") {
-                val serializer = avroBinarySchemaIdSerializer(wireMockEndpoint, isKey = false, includesFormatByte = false)
+                val serializer =
+                  avroBinarySchemaIdSerializer(wireMockEndpoint, isKey = false, includesFormatByte = false)
                 events.foreach(event => serializer.serialize(topic, event))
               }
             }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,19 +1,11 @@
-addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.6.1")
-addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.8.5")
-addSbtPlugin("de.heikoseeberger" % "sbt-header" % "1.6.0")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.7.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-git" % "0.9.3")
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "4.1.0")
 
-addSbtPlugin("me.lessis" % "bintray-sbt" % "0.3.0")
-// The new sbt-bintray plugin suffer from an issue with json4s
-// https://github.com/sbt/sbt-bintray/issues/104
-// addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.4.0")
+addSbtPlugin("org.foundweekends" % "sbt-bintray" % "0.5.2")
 
-addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.5.6")
-addSbtPlugin("com.lucidchart" % "sbt-scalafmt-coursier" % "1.7")
-
-// To resolve custom bintray-sbt plugin
-resolvers += Resolver.url("2m-sbt-plugin-releases", url("https://dl.bintray.com/2m/sbt-plugin-releases/"))(
-  Resolver.ivyStylePatterns
-)
+addSbtPlugin("org.tpolecat" % "tut-plugin" % "0.6.2")
+addSbtPlugin("com.lucidchart" % "sbt-scalafmt-coursier" % "1.15")
 
 resolvers += Resolver.bintrayRepo("tpolecat", "maven")
 
