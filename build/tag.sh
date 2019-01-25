135a10cd20a7d0802f5021517613a7270c1b336a
Filippo De Luca <filippo.deluca@ovoenergy.com>
2019-01-25 11:15:26 +0000

    Update the tag script to avoid tagging an already tagged commit

--- build/tag.sh
+++ build/tag.sh
@@ -6,27 +6,34 @@ echo 'Fetching tag from remote...'
 git tag -l | xargs git tag -d
 git fetch --tags
 
-#get highest tag number
-last_tag=`git describe --abbrev=0 --tags`
-current_version=${last_tag#'v'}
+# TODO Find a way to store the output
+if ! git describe --exact-match 2>/dev/null; then
+  echo 'Not tag found...'
 
-echo "Current version ${current_version}"
+  last_tag=`git describe --abbrev=0 --tags`
+  current_version=${last_tag#'v'}
 
-#replace . with space so can split into an array
-current_version_parts=(${current_version//./ })
+  echo "Current version ${current_version}"
 
-#get number parts and increase last one by 1
-current_version_major=${current_version_parts[0]}
-current_version_minor=${current_version_parts[1]}
-current_version_build=${current_version_parts[2]}
+  #replace . with space so can split into an array
+  current_version_parts=(${current_version//./ })
 
-next_version_build=$((current_version_build+1))
-next_version="$current_version_major.$current_version_minor.$next_version_build"
-next_tag="v${next_version}"
+  #get number parts and increase last one by 1
+  current_version_major=${current_version_parts[0]}
+  current_version_minor=${current_version_parts[1]}
+  current_version_build=${current_version_parts[2]}
 
-echo "Tagging the current commit with ${next_tag}"
+  next_version_build=$((current_version_build+1))
+  next_version="$current_version_major.$current_version_minor.$next_version_build"
+  next_tag="v${next_version}"
 
-git tag -a ${next_tag} -m "Release version "${next_version}
+  echo "Tagging the current commit with ${next_tag}"
 
-echo "Pushing tag ${next_tag} to origin"
-git push origin ${next_tag}
+  git tag -a ${next_tag} -m "Release version "${next_version}
+
+  echo "Pushing tag ${next_tag} to origin"
+  git push origin ${next_tag}
+
+else
+  echo 'Tag found, no tag will be add'
+fi
